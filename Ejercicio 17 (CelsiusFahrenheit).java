caba1185e39aa7b0f11ef842c06d710e1e9eeee2
Aitor Ventura <43577217+Aitor-Ventura@users.noreply.github.com>
2021-02-10 22:11:17 +0000

    Add files via upload

---
+++ Ejercicio 17 (CelsiusFahrenheit).java
@@ -0,0 +1,6 @@
+public class Excercise {
+    public static double convertCelsiusToFahrenheit(double celsiusTemp) {
+        celsiusTemp = (celsiusTemp * 1.8) + 32;
+        return celsiusTemp;
+    }
+}
